7149e12ef47085ca853c936f0a44823e8c81ff13
JINSAN NOH <zingoworks@gmail.com>
2019-05-08 22:35:00 +0900

    Add MessageService

---
+++ src/main/java/com/codesquad/blackjack/service/MessageService.java
@@ -0,0 +1,36 @@
+package com.codesquad.blackjack.service;
+
+import com.codesquad.blackjack.socket.GameSession;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+
+import java.io.IOException;
+
+@Service
+public class MessageService {
+
+    private static final Logger log = LoggerFactory.getLogger(MessageService.class);
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    public <T> void sendToAll(T messageObject, GameSession gameSession) throws IOException {
+        for (WebSocketSession session : gameSession.getSessions()) {
+            send(messageObject, session);
+        }
+    }
+
+    public <T> void send(T messageObject, WebSocketSession session) throws IOException {
+        log.debug("send : {}", session);
+
+        //TODO 예외처리, 동기화필요?
+        TextMessage message = new TextMessage(objectMapper.writeValueAsString(messageObject));
+        session.sendMessage(message);
+    }
+
+}
