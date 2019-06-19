1263f2f783e21f2ed3906f9b7d54a4c0ebca3e8e
JINSAN NOH <zingoworks@gmail.com>
2019-06-19 20:09:11 +0900

    Add test datasource and profile

--- src/main/java/com/codesquad/blackjack/config/WebMvcConfig.java
+++ src/main/java/com/codesquad/blackjack/config/WebMvcConfig.java
@@ -2,15 +2,33 @@ package com.codesquad.blackjack.config;
 
 
 import com.codesquad.blackjack.security.LoginUserHandlerMethodArgumentResolver;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import javax.sql.DataSource;
 import java.util.List;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Bean
+    @Profile("test")
+    public DataSource testDataSource() {
+        DataSourceBuilder factory = DataSourceBuilder
+                .create()
+                .driverClassName("org.h2.Driver")
+                .url("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1");
+        HikariDataSource dataSource = (HikariDataSource) factory.build();
+        dataSource.setPoolName("TEST_H2_DB");
+        dataSource.setMinimumIdle(1);
+        dataSource.setMaximumPoolSize(1);
+        return dataSource;
+    }
 
     @Bean
     public LoginUserHandlerMethodArgumentResolver loginUserArgumentResolver() {
